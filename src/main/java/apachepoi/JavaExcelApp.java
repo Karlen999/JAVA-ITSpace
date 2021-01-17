f89c9e60cd8f8d135bac495ecedcca60b2d1a823
Karlen999 <69694626+Karlen999@users.noreply.github.com>
2021-01-17 19:08:15 +0400

    create row and cell

--- src/main/java/apachepoi/JavaExcelApp.java
+++ src/main/java/apachepoi/JavaExcelApp.java
@@ -1,5 +1,7 @@
 package apachepoi;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -13,6 +15,18 @@ public class JavaExcelApp {
 
         Workbook workbook =new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("mySheet");
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("poxos");
+        Cell cell1 = row.createCell(1);
+        cell1.setCellValue("poxosyan");
+        Row row1 = sheet.createRow(1);
+        Cell cell2 = row1.createCell(0);
+        cell2.setCellValue("petros");
+        Cell cell3 = row1.createCell(1);
+        cell3.setCellValue("petrosyan");
+
+
         FileOutputStream outputStream = new FileOutputStream("G:\\JAVA ITSpace\\src\\main\\resources\\my.xlsx");
 
         workbook.write(outputStream);
